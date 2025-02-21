a1d3c67053bf2ca79dccb082ab66793698849c7e
ikramul <ikramulsohel101@gmail.com>
2025-02-21 23:13:59 +0600

    dynamic url added for api

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,7 @@
-export const ROOT_URL = "http://localhost:7001/api";
-export const ROOT_IMAGE_URL = "http://localhost:7001";
+// local
+// export const ROOT_URL = "http://localhost:7001/api";
+// export const ROOT_IMAGE_URL = "http://localhost:7001";
+
+// deploy
+export const ROOT_URL = "https://news-letter-backend-4sxo.onrender.com/api";
+export const ROOT_IMAGE_URL = "https://news-letter-backend-4sxo.onrender.com";
